52e6df663651192661a274c3852e48ebfd16f68d
Jon Ackers <jonathon_ackers@hotmail.co.uk>
2024-09-14 18:12:45 +0100

    Create RDS and Lambda components

--- cdk/cdk.go
+++ cdk/cdk.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsrds"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -11,19 +17,56 @@ type CdkStackProps struct {
 	awscdk.StackProps
 }
 
-func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) awscdk.Stack {
+const (
+	DATABASE_NAME = "HousePriceDatabase"
+)
+
+func NewHousePricesStack(scope constructs.Construct, id string, props *CdkStackProps) awscdk.Stack {
 	var sprops awscdk.StackProps
 	if props != nil {
 		sprops = props.StackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	// Create a VPC
+	vpc := awsec2.NewVpc(stack, jsii.String("HousePriceVpc"), &awsec2.VpcProps{
+		MaxAzs: jsii.Number(2),
+	})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("CdkQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	// Create a Security Group that allows inbound traffic on port 5432 for PostgreSQL
+	securityGroup := awsec2.NewSecurityGroup(stack, jsii.String("HousePriceSecurityGroup"), &awsec2.SecurityGroupProps{
+		Vpc:              vpc,
+		AllowAllOutbound: jsii.Bool(true),
+	})
+	securityGroup.AddIngressRule(awsec2.Peer_AnyIpv4(), awsec2.Port_Tcp(jsii.Number(5432)), jsii.String("Allow PostgreSQL access"), jsii.Bool(false))
+
+	// Create an RDS instance
+	rdsInstance := awsrds.NewDatabaseInstance(stack, jsii.String("HousePriceRDS"), &awsrds.DatabaseInstanceProps{
+		Engine: awsrds.DatabaseInstanceEngine_Postgres(&awsrds.PostgresInstanceEngineProps{
+			Version: awsrds.PostgresEngineVersion_VER_13(),
+		}),
+		Vpc:            vpc,
+		SecurityGroups: &[]awsec2.ISecurityGroup{securityGroup},
+		InstanceType:   awsec2.InstanceType_Of(awsec2.InstanceClass_T3, awsec2.InstanceSize_MICRO),
+		Credentials:    awsrds.Credentials_FromGeneratedSecret(jsii.String("postgres"), &awsrds.CredentialsBaseOptions{}),
+		DatabaseName:   jsii.String(DATABASE_NAME),
+	})
+
+	// Create Lambda function and grant read access to RDS
+	lambdaFunction := awslambda.NewFunction(stack, jsii.String("HousePriceLambda"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Handler: jsii.String("bootstrap"),
+		Code:    awslambda.Code_FromAsset(jsii.String(filepath.Join("..", "./lambda/")), &awss3assets.AssetOptions{}),
+		Environment: &map[string]*string{
+			"DB_HOST":     rdsInstance.InstanceEndpoint().Hostname(),
+			"DB_NAME":     jsii.String(DATABASE_NAME),
+			"DB_USER":     jsii.String("postgres"),
+			"SECRET_NAME": rdsInstance.Secret().SecretArn(),
+		},
+		Vpc: vpc,
+		SecurityGroups: &[]awsec2.ISecurityGroup{securityGroup},
+	})
+	rdsInstance.Secret().GrantRead(lambdaFunction, jsii.Strings())
 
 	return stack
 }
@@ -33,7 +76,7 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewCdkStack(app, "CdkStack", &CdkStackProps{
+	NewHousePricesStack(app, "HousePricesScraper", &CdkStackProps{
 		awscdk.StackProps{
 			Env: env(),
 		},
@@ -42,29 +85,10 @@ func main() {
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+// env determines the AWS environment (account+region) to deploy the stack in
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+	 Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+	 Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
 }
